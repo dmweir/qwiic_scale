9a7abeae3b5208c9a750410224be7ee037dcefb1
David Weir <dmw109@pitt.edu>
2021-03-05 17:24:28 -0500

    Fixed a bug with default types in arduinoJSON

--- example/qwiic_scale_jsonrpc.ino
+++ example/qwiic_scale_jsonrpc.ino
@@ -32,29 +32,38 @@ int sample_mode = REQUEST;
 
 
 // Dispatches on the requested jsonrpc method
-void dispatch(const uint32_t id, const char *method, const JsonVariant& params) {
-  if (STRCMPI(method, "tare")) {
+void dispatch(const unsigned long id, const char *method, const JsonVariant &params)
+{
+  if (STRCMPI(method, "tare"))
+  {
     tare(id, params);
   }
-  else if (STRCMPI(method, "calibrate")) {
+  else if (STRCMPI(method, "calibrate"))
+  {
     calibrate(id, params);
   }
-  else if (STRCMPI(method, "set_calibration")) {
-    set_calibration(id, params);
+  else if (STRCMPI(method, "reset_calibration"))
+  {
+    reset_calibration(id, params);
   }
-  else if (STRCMPI(method, "get_calibration")) {
+  else if (STRCMPI(method, "get_calibration"))
+  {
     get_calibration(id, params);
   }
-  else if (STRCMPI(method, "get_average_weight")) {
+  else if (STRCMPI(method, "get_average_weight"))
+  {
     get_average_weight(id, params);
   }
-  else if (STRCMPI(method, "get_average_reading")) {
+  else if (STRCMPI(method, "get_average_reading"))
+  {
     get_average_reading(id, params);
   }
-  else if (STRCMPI(method, "get_status")) {
+  else if (STRCMPI(method, "get_status"))
+  {
     get_status(id, params);
   }
-  else if (STRCMPI(method, "get_sensors")) {
+  else if (STRCMPI(method, "get_sensors"))
+  {
     get_sensors(id, params);
   }
   //  else if (STRCMPI(method, "reset")) {
@@ -66,34 +75,40 @@ void dispatch(const uint32_t id, const char *method, const JsonVariant& params) 
   //  else if (STRCMPI(method, "power_down")) {
   //    power_down(id, params);
   //  }
-  else if (STRCMPI(method, "change_mode")) {
+  else if (STRCMPI(method, "change_mode"))
+  {
     change_mode(id, params);
   }
-  else {
+  else
+  {
     jsonrpc_method_not_found(id);
   }
 }
 
 // Scale RPC Methods
 // All Methods must have the signature void f(uint32_t id, const JsonVariant& params)
-void calibrate(const uint32_t id, const JsonVariant& params) {
+void calibrate(const unsigned long id, const JsonVariant &params)
+{
   StaticJsonDocument<256> reply;
 
-  float weight = params["calibration_weight"] | -1;
-  int num_readings = params["average_size"] | -1;
+  float weight = params["calibration_weight"] | -1.0f;
+  long num_readings = params["average_size"] | -1L;
 
-  if ((weight < 1) || (weight > 500)) {
+  if ((weight < 1) || (weight > 500))
+  {
     jsonrpc_invalid_params(id, F("By-name parameter 'weight' is missing or outside range."));
     return;
   }
-  else if ((num_readings < 1) || (num_readings > 255)) {
-    jsonrpc_invalid_params(id, F("By-name parameter 'average_size' is missing or > 255."));
+  else if ((num_readings < 1) || (num_readings > 64))
+  {
+    jsonrpc_invalid_params(id, F("By-name parameter 'average_size' is missing or > 64."));
     return;
   }
 
   error_code_t err = Scale.calculateCalibrationFactor(weight, num_readings);
 
-  if (!err) {
+  if (!err)
+  {
     reply["id"] = id;
     JsonObject result = reply.createNestedObject("result");
     result["timestamp"] = millis();
@@ -101,25 +116,29 @@ void calibrate(const uint32_t id, const JsonVariant& params) {
     result["zero_offset"] = Scale.getZeroOffset();
     serializeJson(reply, Serial);
     Serial.println();
-  } else {
+  }
+  else
+  {
     jsonrpc_scale_error(id, err);
   }
-
 }
 
 // Tare the scale so that current value is new zero point.
-void tare(const uint32_t id, const JsonVariant& params) {
+void tare(const unsigned long id, const JsonVariant &params)
+{
   StaticJsonDocument<128> reply;
-  int num_readings = params["average_size"] | -1;
+  long num_readings = params["average_size"] | -1L;
 
-  if ((num_readings < 1) || (num_readings > 255)) {
-    jsonrpc_invalid_params(id, F("By-name parameter 'average_size' is missing or > 255."));
+  if ((num_readings < 1) || (num_readings > 64))
+  {
+    jsonrpc_invalid_params(id, F("By-name parameter 'average_size' is missing or > 64."));
     return;
   }
 
   error_code_t err = Scale.calculateZeroOffset(num_readings);
 
-  if (!err) {
+  if (!err)
+  {
     reply["id"] = id;
     JsonObject result = reply.createNestedObject("result");
     result["timestamp"] = millis();
@@ -127,18 +146,22 @@ void tare(const uint32_t id, const JsonVariant& params) {
     result["zero_offset"] = Scale.getZeroOffset();
     serializeJson(reply, Serial);
     Serial.println();
-  } else {
+  }
+  else
+  {
     jsonrpc_scale_error(id, err);
   }
 }
 
 // Change the mode the microcontroller
-void change_mode(const uint32_t id, const JsonVariant& params) {
+void change_mode(const unsigned long id, const JsonVariant &params)
+{
 
   StaticJsonDocument<128> reply;
-  const char* mode = params["mode"] | "invalid";
+  const char *mode = params["mode"] | "invalid";
 
-  if (STRCMPI(mode, "invalid")) {
+  if (STRCMPI(mode, "invalid"))
+  {
     jsonrpc_invalid_params(id, F("By-name parameter 'mode' is missing or invalid"));
     return;
   }
@@ -150,12 +173,17 @@ void change_mode(const uint32_t id, const JsonVariant& params) {
   jsonrpc_ack(id);
 }
 
-void set_calibration(const uint32_t id, const JsonVariant& params) {
+void reset_calibration(const unsigned long id, const JsonVariant &params)
+{
   float cal_factor = params["cal_factor"] | 1.0f;
-  long zero_offset = params["zero_offset"] | 0;
+  long zero_offset = params["zero_offset"] | 0L;
+  bool cal_state = params["is_calibrated"] | false;
 
   Scale.setCalibrationFactor(cal_factor);
   Scale.setZeroOffset(zero_offset);
+  Scale.isCalibrated = cal_state;
+  Scale.calibrationDetected = false;
+  Scale.storeCalibration();
 
   StaticJsonDocument<128> reply;
   reply["id"] = id;
@@ -167,7 +195,8 @@ void set_calibration(const uint32_t id, const JsonVariant& params) {
   Serial.println();
 }
 
-void get_calibration(const uint32_t id, const JsonVariant& params) {
+void get_calibration(const unsigned long id, const JsonVariant &params)
+{
   StaticJsonDocument<128> reply;
   reply["id"] = id;
   JsonObject result = reply.createNestedObject("result");
@@ -178,19 +207,22 @@ void get_calibration(const uint32_t id, const JsonVariant& params) {
   Serial.println();
 }
 
-void get_average_reading(const uint32_t id, const JsonVariant& params) {
+void get_average_reading(const unsigned long id, const JsonVariant &params)
+{
   StaticJsonDocument<128> reply;
-  int num_readings = params["average_size"] | -1;
+  long num_readings = params["average_size"] | -1L;
 
-  if ((num_readings < 1) || (num_readings > 255)) {
-    jsonrpc_invalid_params(id, F("By-name parameter 'average_size' is missing or > 255."));
+  if ((num_readings < 1) || (num_readings > 64))
+  {
+    jsonrpc_invalid_params(id, F("By-name parameter 'average_size' is missing or > 64."));
     return;
   }
 
   int32_t avg_reading;
   error_code_t err = Scale.getAverageReading(&avg_reading, num_readings);
 
-  if (!err) {
+  if (!err)
+  {
     reply["id"] = id;
     JsonObject result = reply.createNestedObject("result");
     result["timestamp"] = millis();
@@ -198,25 +230,30 @@ void get_average_reading(const uint32_t id, const JsonVariant& params) {
     result["num_samples"] = num_readings;
     serializeJson(reply, Serial);
     Serial.println();
-  } else {
+  }
+  else
+  {
     jsonrpc_scale_error(id, err);
   }
 }
 
-void get_average_weight(const uint32_t id, const JsonVariant& params) {
+void get_average_weight(const unsigned long id, const JsonVariant &params)
+{
   StaticJsonDocument<128> reply;
-  int num_readings = params["average_size"] | -1;
+  long num_readings = params["average_size"] | -1L;
   bool allow_negative = params["allow_negative"] | true;
 
-  if ((num_readings < 1) || (num_readings > 255)) {
-    jsonrpc_invalid_params(id, F("By-name parameter 'num_readings' is missing or > 255."));
+  if ((num_readings < 1) || (num_readings > 64))
+  {
+    jsonrpc_invalid_params(id, F("By-name parameter 'num_readings' is missing or > 64."));
     return;
   }
 
   float avg_weight;
   error_code_t err = Scale.getAverageWeight(&avg_weight, num_readings, allow_negative);
 
-  if (!err) {
+  if (!err)
+  {
     reply["id"] = id;
     JsonObject result = reply.createNestedObject("result");
     result["timestamp"] = millis();
@@ -224,12 +261,15 @@ void get_average_weight(const uint32_t id, const JsonVariant& params) {
     result["num_samples"] = num_readings;
     serializeJson(reply, Serial);
     Serial.println();
-  } else {
+  }
+  else
+  {
     jsonrpc_scale_error(id, err);
   }
 }
 
-void get_status(const uint32_t id, const JsonVariant& params) {
+void get_status(const unsigned long id, const JsonVariant &params)
+{
   StaticJsonDocument<128> reply;
   reply["id"] = id;
   JsonObject result = reply.createNestedObject("result");
@@ -241,13 +281,15 @@ void get_status(const uint32_t id, const JsonVariant& params) {
   Serial.println();
 }
 
-void get_sensors(uint32_t id, const JsonVariant& params) {
+void get_sensors(uint32_t id, const JsonVariant &params)
+{
   float avg_weight;
   StaticJsonDocument<128> reply;
 
   error_code_t err = Scale.getAverageWeight(&avg_weight, AVG_SIZE);
 
-  if (!err) {
+  if (!err)
+  {
     reply["id"] = id;
     JsonObject result = reply.createNestedObject("result");
     result["timestamp"] = millis();
@@ -255,20 +297,24 @@ void get_sensors(uint32_t id, const JsonVariant& params) {
     result["num_samples"] = AVG_SIZE;
     serializeJson(reply, Serial);
     Serial.println();
-  } else {
+  }
+  else
+  {
     jsonrpc_scale_error(SERVER_ID, err);
   }
 }
 
 // Continuous Streaming Mode
-void stream_sensors(void) {
+void stream_sensors(void)
+{
   static bool streaming_error;
   float avg_weight;
   StaticJsonDocument<128> reply;
 
   error_code_t err = Scale.getAverageWeight(&avg_weight, AVG_SIZE);
 
-  if (!err) {
+  if (!err)
+  {
     streaming_error = false;
     reply["id"] = SERVER_ID;
     JsonObject result = reply.createNestedObject("result");
@@ -277,9 +323,12 @@ void stream_sensors(void) {
     result["num_samples"] = AVG_SIZE;
     serializeJson(reply, Serial);
     Serial.println();
-  } else {
+  }
+  else
+  {
     // Only send the first error encountered
-    if (!streaming_error) {
+    if (!streaming_error)
+    {
       jsonrpc_scale_error(SERVER_ID, err);
       streaming_error = true;
     }
@@ -287,7 +336,8 @@ void stream_sensors(void) {
 }
 
 // Acknowledgement Response
-void jsonrpc_ack(const uint32_t id) {
+void jsonrpc_ack(const unsigned long id)
+{
   StaticJsonDocument<128> reply;
   reply["id"] = id;
   JsonObject result = reply.createNestedObject("result");
@@ -297,7 +347,8 @@ void jsonrpc_ack(const uint32_t id) {
 }
 
 // Error Response Helper Functions
-void jsonrpc_scale_error(uint32_t id, error_code_t scale_err) {
+void jsonrpc_scale_error(uint32_t id, error_code_t scale_err)
+{
   StaticJsonDocument<128> reply;
   JsonObject err = reply.createNestedObject("error");
   err["timestamp"] = millis();
@@ -308,10 +359,10 @@ void jsonrpc_scale_error(uint32_t id, error_code_t scale_err) {
   Serial.println();
 }
 
-void jsonrpc_parse_error(const DeserializationError& error) {
+void jsonrpc_parse_error(const DeserializationError &error)
+{
   StaticJsonDocument<128> reply;
   JsonObject err = reply.createNestedObject("error");
-  err["timestamp"] = millis();
   err["code"] = PARSE_ERROR;
   err["message"] = F("Error parsing received JSON.");
   err["data"] = error.f_str();
@@ -319,20 +370,20 @@ void jsonrpc_parse_error(const DeserializationError& error) {
   Serial.println();
 }
 
-void jsonrpc_invalid_request(void) {
+void jsonrpc_invalid_request(void)
+{
   StaticJsonDocument<128> reply;
   JsonObject err = reply.createNestedObject("error");
-  err["timestamp"] = millis();
   err["code"] = INVALID_REQUEST;
   err["message"] = F("The JSON sent is not a valid Request object.");
   serializeJson(reply, Serial);
   Serial.println();
 }
 
-void jsonrpc_method_not_found(const uint32_t id) {
+void jsonrpc_method_not_found(const unsigned long id)
+{
   StaticJsonDocument<128> reply;
   JsonObject err = reply.createNestedObject("error");
-  err["timestamp"] = millis();
   err["code"] = METHOD_NOT_FOUND;
   err["message"] = F("The method does not exist.");
   reply["id"] = id;
@@ -340,11 +391,11 @@ void jsonrpc_method_not_found(const uint32_t id) {
   Serial.println();
 }
 
-void jsonrpc_invalid_params(const uint32_t id, const __FlashStringHelper* data) {
+void jsonrpc_invalid_params(const unsigned long id, const __FlashStringHelper *data)
+{
   StaticJsonDocument<128> reply;
   JsonObject err = reply.createNestedObject("error");
-  err["timestamp"] = millis();
-  err["code"] = METHOD_NOT_FOUND;
+  err["code"] = INVALID_PARAMS;
   err["message"] = F("Invalid method parameter(s).");
   err["data"] = data;
   reply["id"] = id;
@@ -353,12 +404,14 @@ void jsonrpc_invalid_params(const uint32_t id, const __FlashStringHelper* data) 
 }
 
 // INITIALIZATION (ONLY RUNS ONCE AT THE BEGINNING)
-void setup() {
+void setup()
+{
   error_code_t err;
 
   Serial.begin(BAUDRATE, SERIAL_8N1);
 
-  while (!Serial) continue; //Freeze
+  while (!Serial)
+    continue; //Freeze
   Serial.flush();
   Serial.setTimeout(100);
 
@@ -366,14 +419,16 @@ void setup() {
 
   StaticJsonDocument<256> reply;
   err = Scale.begin();
-  if (err) {
+  if (err)
+  {
     jsonrpc_scale_error(SERVER_ID, err);
     FREEZE
   }
 
   // Configure Scale
   err = Scale.setSampleRate(NAU7802_SPS_80);
-  if (err) {
+  if (err)
+  {
     jsonrpc_scale_error(SERVER_ID, err);
     FREEZE
   }
@@ -381,40 +436,56 @@ void setup() {
   // Internal calibration.
   // Recommended after power up, gain changes, sample rate changes, or channel change
   err = Scale.calibrateAFE();
-  if (err) {
+  if (err)
+  {
     jsonrpc_scale_error(SERVER_ID, err);
     FREEZE
   }
 
   // Load zeroOffset and calibrationFactor from EEPROM
   err = Scale.readCalibration();
-  if (err) {
+  if (err)
+  {
     jsonrpc_scale_error(SERVER_ID, err);
   }
 }
 // MAIN LOOP (CALLED INDEFINITELY)
-void loop() {
+void loop()
+{
 
   StaticJsonDocument<128> request;
 
-  if (Serial.available()) {
-    DeserializationError err = deserializeJson(request, Serial);
-    if (err) {
+  if (Serial.available())
+  {
+    String request_line = Serial.readStringUntil('\n');
+    DeserializationError err = deserializeJson(request, request_line);
+    if (err)
+    {
       jsonrpc_parse_error(err);
+      // flush any remaining input
+//      while (Serial.available())
+//        Serial.read();
       return;
     }
-
+    //flush any remaining input
+//    while (Serial.available())
+//      Serial.read();
     // Parse the request
     const char *method = request["method"] | "?";
     JsonVariant params = request["params"];
-    uint32_t id = request["id"] | 0;
+    unsigned long  id = request["id"] | 0uL;
 
-    if ((strcmp(method, "?") != 0) && (id > 0)) {
+    if ((strcmp(method, "?") != 0) && (id > 0))
+    {
       dispatch(id, method, params);
-    } else {
+    }
+    else
+    {
       jsonrpc_invalid_request();
     }
-  } else if (sample_mode == CONTINUOUS) {
+  }
+  else if (sample_mode == CONTINUOUS)
+  {
     stream_sensors();
   }
 }
